7ac9e27202ca1c50c7c7a823b9acc171f5337936
tkhang10 <truongtuongvykhang2000@gmail.com>
2024-03-23 17:08:32 +0700

    update env

--- mainTrain.py
+++ mainTrain.py
@@ -11,7 +11,7 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.utils import to_categorical
 
-image_directory='Br35H-Mask-RCNN\TRAIN'
+image_directory='Dataset/'
 
 no_tumor_images=os.listdir(image_directory+ 'no')
 yes_tumor_images=os.listdir(image_directory+ 'yes')
