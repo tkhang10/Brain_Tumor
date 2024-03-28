a51609e516e38ead33079d00d97f559477a9ff33
tkhang10 <truongtuongvykhang2000@gmail.com>
2024-03-28 18:41:18 +0700

    was draw

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import cv2
 from tensorflow.keras.preprocessing import image
 from PIL import Image
 from keras.models import load_model
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_from_directory
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
@@ -30,6 +30,12 @@ def get_result(filepath):
     result = model.predict(img)
     return result
 
+# Function to draw tumor box on the image
+def draw_tumor_box(image, box_coordinates):
+    x1, y1, x2, y2 = box_coordinates
+    cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), 2)  
+    return image
+
 # Home page
 @app.route('/', methods=['GET'])
 def index():
@@ -43,6 +49,10 @@ def allowed_file(filename):
 # Define upload folder
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 # Prediction route
 @app.route('/predict', methods=['POST'])
@@ -63,10 +73,37 @@ def upload():
 
     file.save(filepath)
 
+    # Get prediction result
     value = get_result(filepath)
     result = get_class_name(value)
-    os.remove(filepath)  # remove the file after processing
-    return result
+    
+    if result == "This is a Brain Tumor":
 
+        # Load the image to draw tumor box
+        image = cv2.imread(filepath)
+
+        # Example coordinates of the tumor box
+        box_coordinates = (100,100,200,200)
+
+        # Draw tumor box on the image
+        image_with_tumor = draw_tumor_box(image.copy(), box_coordinates)
+
+        # Save the image with tumor box
+        new_filename = 'colorize_' + secure_filename(file.filename)
+        image_with_tumor_path = os.path.join(app.config['UPLOAD_FOLDER'], new_filename)
+        cv2.imwrite(image_with_tumor_path, image_with_tumor)
+
+        # Remove the uploaded file after processing (optional)
+        # os.remove(filepath)
+
+        # Return the result and the image
+        return render_template('index.html', result=result, image_path=image_with_tumor_path)
+    
+    else:
+        # Return only the result
+        return result
+    
+
+    
 if __name__ == '__main__':
     app.run(debug=True)
