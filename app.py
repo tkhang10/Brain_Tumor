ef83f6cc1c036a15625ee59911f876debd1e3b0a
tkhang10 <truongtuongvykhang2000@gmail.com>
2024-03-28 15:32:58 +0700

    before colorize

--- app.py
+++ app.py
@@ -1,60 +1,72 @@
 import os
 import tensorflow as tf
 import numpy as np
+import cv2
 from tensorflow.keras.preprocessing import image
 from PIL import Image
-import cv2
 from keras.models import load_model
 from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
 
-
 app = Flask(__name__)
 
-
-model =load_model('BrainTumor10EpochsCategorical.h5')
+# Load the pre-trained model
+model = load_model('BrainTumor10EpochsCategorical.h5')
 print('Model loaded. Check http://127.0.0.1:5000/')
 
+# Function to get class name based on prediction
+def get_class_name(classNo):
+    if classNo[0][0] < 1:
+        return "This is a Brain Tumor"
+    else:
+        return "This is not a Brain Tumor"
 
-def get_className(classNo):
-	print(classNo)
-	print(classNo[0])
-	if classNo[0][0]<1:
-		return "This is a Brain Tumor"
-	else:
-		return "This is not a Brain Tumor"
-
-
-def getResult(img):
-    img = Image.open(image)
+# Function to get prediction result from image
+def get_result(filepath):
+    img = Image.open(filepath)
     img = img.resize((64, 64))
-    img = np.array(img) / 255.0  # Chuẩn hóa ảnh
+    img = np.array(img) / 255.0  # Normalize the image
     img = np.expand_dims(img, axis=0)
     result = model.predict(img)
     return result
 
-
+# Home page
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
+# Set the allowed file extensions
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/predict', methods=['GET', 'POST'])
+# Define upload folder
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Prediction route
+@app.route('/predict', methods=['POST'])
 def upload():
-    if request.method == 'POST':
-        file = request.files['file']
+    file = request.files['file']
 
-        if file:
-            filename = secure_filename(file.filename)
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(filepath)
+    if not file:
+        return 'No file specified.'
 
-            value = getResult(filepath)
-            result = get_className(value)
-            os.remove(filepath) # remove the file after processing
-            return result
+    if not allowed_file(file.filename):
+        return 'Invalid file extension. Only PNG, JPG, and JPEG are allowed.'
 
-    return None
+    filename = secure_filename(file.filename)
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+
+    file.save(filepath)
+
+    value = get_result(filepath)
+    result = get_class_name(value)
+    os.remove(filepath)  # remove the file after processing
+    return result
 
 if __name__ == '__main__':
     app.run(debug=True)
